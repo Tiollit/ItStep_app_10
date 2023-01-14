d93e882ff8c38bd615d1a255c897a17c3c13e5d1
Tiollit <Tiollit7@gmail.com>
2023-01-14 08:23:03 -0800

    g

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <string.h>
+#include <stdio.h>
 using namespace std;
+
 int main()
 {
-    fstream file;
-    string text;
+    ofstream file;
+    string inf;
+    cout << "Insert Text to a File" << endl;
+    gets_s(inf);
     file.open("file.txt", ios::out);
     if (!file.is_open())
     {
@@ -13,8 +18,8 @@ int main()
     }
     else
     {
-        gets_s("file.txt");
-        file.close();
+        file << inf << endl;        
     }
+
     return 0;
 }
